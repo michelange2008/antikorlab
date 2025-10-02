ad7d64e24d5d90900feb4915a3812209f256c5a7
Michel BOUY <michelange@wanadoo.fr>
2025-10-02 18:38:04 +0200

    Mise a jour vers Laravel 10 et infos légales

--- resources/lang/fr/infoslegales.php
+++ resources/lang/fr/infoslegales.php
@@ -2,18 +2,16 @@
 
 return array (
   'titre' => 'Informations légales',
-  'soustitre' => 'Le site parasitlab.org est édité par le FiBL France',
+  'soustitre' => 'Le site lab.antikor.fr est édité par Antikor Scop',
   'bloc_1' =>
   array (
     'titre' => 'Editeur',
     'contenu' =>
     array (
-      'p1' => '<strong>FiBL France&nbsp;</strong>: Association Loi 1901 déclarée à la préfecture de Valence (Drôme)',
-      'p2' => '<i>Adresse&nbsp;:</i> Pôle Bio - Ecosite du Val de Drôme
-150 avenue de Judée
-F-26400 Eurre',
-      'p3' => '<i>Téléphone&nbsp;:</i> +33 (0) 4 75 25 41 ',
-      'p4' => '<i>Directeur de la publication&nbsp;:</i> Félix Heckendorn - Chercheur en parasitologie',
+      'p1' => '<strong>Antikor Scop&nbsp;</strong>: SCOP à responsabilité limitée (49799851800011 RCS Romans/Isère)',
+      'p2' => '<i>Adresse&nbsp;:</i> 605 Grande Rue - 26300 BARBIERES',
+      'p3' => '<i>Téléphone&nbsp;:</i> +33 (0) 4 75 4720 35 ',
+      'p4' => '<i>Directeur de la publication&nbsp;:</i> Fabrice RIGAL Gérant',
       'p5' => '<i>Co-directeur de la publication&nbsp;:</i> Michel BOUY - Vétérinaire',
     ),
   ),
@@ -22,9 +20,9 @@ F-26400 Eurre',
     'titre' => 'Hébergement',
     'contenu' =>
     array (
-      'p1' => '<strong>OVH</strong>',
-      'p2' => '<i>Adresse&nbsp;:</i> 2rue Kellerman - F-59100 Roubaix',
-      'p3' => '<i>Site Web&nbsp;:</i> <a class=\'color-bleu\' href=\'https://www.ovhcloud.com/fr/about-us/\'>https://www.ovhcloud.com/fr/about-us/</a>,',
+      'p1' => '<strong>INFOMANIAK</strong>',
+      'p2' => '<i>Adresse&nbsp;:</i> Les Acacias, Genève, Suisse',
+      'p3' => '<i>Site Web&nbsp;:</i> <a class=\'color-bleu\' href=\'https://www.infomaniak.com/fr\'>https://www.infomaniak.com/fr</a>,',
     ),
   ),
   'bloc_3' =>
@@ -51,7 +49,7 @@ F-26400 Eurre',
     'titre' => 'Mention relative aux cookies',
     'contenu' =>
     array (
-      'p1' => 'Le site parasitlab.org utilise uniquement des cookies indispensables au fonctionnement et à la sécurité du site',
+      'p1' => 'Le site lab.antikor.org utilise uniquement des cookies indispensables au fonctionnement et à la sécurité du site',
       'p2' => 'Ces cookies sont constitués d\'une série de chiffres et de lettres et ne contiennent aucune information de nature à tracer l\'internaute. Ces cookies ont une durée de validité de 2 heures',
       'p3' => 'Ces cookies servent à vérifier à chaque changement de page qu\'il n\'y a pas eu d\'intrusion dans les échanges entre le serveur et l\'internaute. Lorsqu\'un utilisateur est connecté, un cookie permet d\'éviter que le site ne lui demande de se réidentifier à chaque changement de page.',
     ),
@@ -61,7 +59,7 @@ F-26400 Eurre',
     'titre' => 'Mentions relatives à l\'utilisation de données personnelles',
     'contenu' =>
     array (
-      'p1' => '<i>Personne contact&nbsp;:</i> Elina HARINCK - +33 (0) 4 75 24 41.',
+      'p1' => '<i>Personne contact&nbsp;:</i> Michel BOUY - +33 (0) 4 75 47 20 35',
       'p2' => '<i>Traitement des données&nbsp;:</i> Le principal traitement de données consiste à envoyer au demandeur et éventuellement à son vétérinaire les résultats des analyses effectuées. Ces résultats sont confidentiels et ne sont connus que des membres du laboratoire, du demandeur et éventuellement de son vétérinaire. Pour les besoins de la recherche, le FiBL France se réserve la possibilité d\'une exploitation statistique des résultats d\'analyses après anonymisation de ces données.',
       'p3' => '<i>Destinataires des données&nbsp;:</i> Aucune donnée personnelle (nom, adresse, email, téléphone, numéro d\'ede ou d\'Ordre) ne sera transmise à un tiers, ni dans l\'Union Européenne ni en dehors.',
       'p4' => '<i>Droits d\'opposition, d\'interrogation, d\'accès et de rectification&nbsp;:</i> Tout utilisateur peut faire valoir ses droits en contactant le FiBL France à l\'adresse suivante: info.france@fibl.org.',
